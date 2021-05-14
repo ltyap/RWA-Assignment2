4aac354d92514b31f35366ecca99a9d78db92665
UlaCebula <u.golyska@gmail.com>
2021-05-14 16:14:10 +0200

    Setting up vortex system
    Fixed setting up vortex system

--- main.m
+++ main.m
@@ -1,3 +1,6 @@
+%% TO DO
+% add control points from all blades, not just one
+
 % Assignment 2 of RWA - lifting line model
 clc
 clear all
@@ -28,7 +31,7 @@ twist_distribution = -14*(1-r_R)+pitch; % degrees
 Uinf = 10;      % freestream velocity
 rho = 1.225;    % air density
 pinf = 101300;  % atmospheric pressure
-TSR = 8;    %TSR_array =[6,8,10]; % tip speed ratios we want to calculate
+TSR = 8;        % tip speed ratios we want to calculate
 Radius = 50;    % blade Radius
 NBlades = 3;    % number of blades
 a_wake = 0.2602;   % ,should be average induction at the rotor, from BEM
@@ -36,102 +39,82 @@ Uwake = Uinf*(1-a_wake);
 
 Omega = Uinf*TSR/Radius;
 Lw_D = 0.2;    % wake length in diameters downstream
-% determine trailing vortices
-for i=1:N+1
-    trail_x1 = 0 + chord_distribution(i)*sin(deg2rad(-twist_distribution(i)));
-    trail_y1 = chord_distribution(i)/4 + chord_distribution(i)*cos(deg2rad(-twist_distribution(i)));
-    trail_z1 = r_R(i)*Radius + 0;
-    
-    %ts_func = @(ts) sqrt(ts^2*Uwake^2+r_R(i)^2*Radius^2*sin(Omega*ts)^2+r_R(i)^2*Radius^2*cos(Omega*ts)^2)-chord_distribution(i) ;
-    %ts = fsolve(ts_func,0.1);  % step so that we have wake discretized into chord lengths
-    ts = 0.2; % maybe come up with a function for determining this
-    t = [0:ts:Lw_D*2*Radius/Uwake];
-    x = t*Uwake;
-    y = r_R(i)*Radius*sin(Omega*t);
-    z = r_R(i)*Radius*cos(Omega*t);
-    trail_points.x(i,:) = [trail_x1, trail_x1+x];
-    trail_points.y(i,:) = [trail_y1, trail_y1+y]; %[trail_y1, trail_y1+y];
-    trail_points.z(i,:) = [trail_z1, z]; %[trail_z1, trail_z1+z];
-end
 
-% determine control point locations - at middle of segments
-c = (chord_distribution(1:end-1)+chord_distribution(2:end))/2;  % local chord
-cp(:,1) = zeros(N,1); % x-coordinate
-cp(:,2) = c/4;     % y-coordinate
-cp(:,3) = (r_R(1:end-1)+r_R(2:end))/2*Radius;     % z-coordinate
-cp_twist = interp1(r_R, twist_distribution, cp(:,3)/Radius);% twist at control points
+theta_array = [0:2*pi/NBlades:2*pi*(1-1/NBlades)]; % azimuthal positions of blades, assuming the first one is at 0
+
+[cp, bound, trail]=vortex_system(Omega, NBlades, theta_array,N, r_R, Radius, twist_distribution, chord_distribution, Lw_D, Uwake)
 
 %% determine induced velocity per unit strength of circulation (for Gamma=1)
-for i=1:length(cp(:,1))
-    for j=1:length(r_R)-1
-        induced_vel=[0,0,0];
-        
-        point1 = [0,chord_distribution(j)/4,r_R(j)*Radius]; % coordinates of start of bound vortex
-        point2 = [0,chord_distribution(j+1),r_R(j+1)*Radius]; % coordinates of end of bound vortex
-        temp = induced_v_from_vortex(1,point1, point2, cp(i,:));
-        induced_vel=induced_vel+temp;
-        
-        % first trailing vortex
-        temp = induced_v_from_vortex(1,[trail_points.x(j,1),trail_points.y(j,1),trail_points.z(j,1)] ,point1, cp(i,:));
-        induced_vel = induced_vel+temp;
-        
-        temp = induced_v_from_vortex(1, point2, [trail_points.x(j+1,1),trail_points.y(j+1,1),trail_points.z(j+1,1)], cp(i,:));
-        induced_vel = induced_vel+temp;        
-        
-        for k=2:length(trail_points.x(i,:))-1 % for the whole length of wake - positive circulation
-            temp = induced_v_from_vortex(1,[trail_points.x(j,k+1),trail_points.y(j,k+1),trail_points.z(j,k+1)] ,[trail_points.x(j,k),trail_points.y(j,k),trail_points.z(j,k)], cp(i,:));
-            induced_vel = induced_vel+temp;
-        end
-        
-        for k=2:length(trail_points.x(i,:))-1 % for the whole length of wake - negative circulation
-            temp = induced_v_from_vortex(1,[trail_points.x(j+1,k),trail_points.y(j+1,k),trail_points.z(j+1,k)] ,[trail_points.x(j+1,k+1),trail_points.y(j+1,k+1),trail_points.z(j+1,k+1)], cp(i,:));
-            induced_vel = induced_vel+temp;
-        end
-        
-        % write into influence matrix - for unit gamma
-        Influence_u(i,j) = induced_vel(1);
-        Influence_v(i,j) = induced_vel(2);
-        Influence_w(i,j) = induced_vel(3);
-    end
-end
-
-%% check if the system if set up correctly
-figure('visible', 'on')
-hold on
-plot3( zeros(1,2*(N+1)), [zeros(1,N+1),flip(chord_distribution)], [r_R*Radius,flip(r_R*Radius)])% blade outline
-scatter3(cp(:,1), cp(:,2), cp(:,3), 15, 'r', 'filled')  % control points
-plot3(zeros(1,N+1),chord_distribution/4,r_R*Radius,'-+', 'Color', 'k')   % bound vortices
-for i=1:N+1
-    plot3([0, trail_points.x(i,:)], [chord_distribution(i)/4,trail_points.y(i,:)],[r_R(i)*Radius,trail_points.z(i,:)], 'k')
-end
-% axis equal
-xlabel("x")
-ylabel("y")
-zlabel("z")
-hold off
-
-%% Calculate
-%why do we also have r_R as an output?
-[a, aline, r_R, Fnorm, Ftan, Gamma] = solveGamma(Uinf, N, Radius, cp, Influence_u, Influence_v, Influence_w, Omega, polar_alpha, polar_cl, polar_cd, cp_twist);
-
-%% Post processing
-temp = RootLocation_R + flip(b*(cos(theta)+1));  
-dr = temp(2:end)-temp(1:end-1); % lengths of sections
-
-CT_sections = Fnorm*NBlades.*dr/(0.5*Uinf^2*pi*Radius^2); % local thrust coefficient
-CQ_sections = Ftan.*r_R*NBlades.*dr*Radius/(0.5*Uinf^3*pi*Radius^2);    % local torque coefficient
-CP_sections = Ftan.*r_R*NBlades.*dr*Radius*Omega/(0.5*Uinf^3*pi*Radius^2); % local power coefficient  P=omega*Q
-Gamma = Gamma/(pi*Uinf^2/(NBlades*Omega)); % non-dimensionalize circulation
-
-CT = sum(dr.*Fnorm*NBlades/(0.5*Uinf^2*pi*Radius^2));     % total thrust coefficient
-CP = sum(dr'.*Ftan.*r_R*NBlades*Radius*Omega/(0.5*Uinf^3*pi*Radius^2));    % total power coefficient
-
-CT_all = sum(CT_sections, 'all');  % write to array of thrust coefficients
-CQ_all = sum(CQ_sections, 'all');  % write to array of torque coefficients
-CP_all = sum(CP_sections, 'all');  % write to array of power coefficients
-
-%%%%%% SHOULD BE %%%%%%%%%
-% CT = 0.6553;
-% CQ = 0.2801;
-% CP = 0.4482;
+% for i=1:length(cp(:,1))
+%     for j=1:length(r_R)-1
+%         induced_vel=[0,0,0];
+%         
+%         point1 = [0,chord_distribution(j)/4,r_R(j)*Radius]; % coordinates of start of bound vortex
+%         point2 = [0,chord_distribution(j+1),r_R(j+1)*Radius]; % coordinates of end of bound vortex
+%         temp = induced_v_from_vortex(1,point1, point2, cp(i,:));
+%         induced_vel=induced_vel+temp;
+%         
+%         % first trailing vortex
+%         temp = induced_v_from_vortex(1,[trail_points.x(j,1),trail_points.y(j,1),trail_points.z(j,1)] ,point1, cp(i,:));
+%         induced_vel = induced_vel+temp;
+%         
+%         temp = induced_v_from_vortex(1, point2, [trail_points.x(j+1,1),trail_points.y(j+1,1),trail_points.z(j+1,1)], cp(i,:));
+%         induced_vel = induced_vel+temp;        
+%         
+%         for k=2:length(trail_points.x(i,:))-1 % for the whole length of wake - positive circulation
+%             temp = induced_v_from_vortex(1,[trail_points.x(j,k+1),trail_points.y(j,k+1),trail_points.z(j,k+1)] ,[trail_points.x(j,k),trail_points.y(j,k),trail_points.z(j,k)], cp(i,:));
+%             induced_vel = induced_vel+temp;
+%         end
+%         
+%         for k=2:length(trail_points.x(i,:))-1 % for the whole length of wake - negative circulation
+%             temp = induced_v_from_vortex(1,[trail_points.x(j+1,k),trail_points.y(j+1,k),trail_points.z(j+1,k)] ,[trail_points.x(j+1,k+1),trail_points.y(j+1,k+1),trail_points.z(j+1,k+1)], cp(i,:));
+%             induced_vel = induced_vel+temp;
+%         end
+%         
+%         % write into influence matrix - for unit gamma
+%         Influence_u(i,j) = induced_vel(1);
+%         Influence_v(i,j) = induced_vel(2);
+%         Influence_w(i,j) = induced_vel(3);
+%     end
+% end
+% 
+% %% check if the system if set up correctly
+% figure('visible', 'on')
+% hold on
+% plot3( zeros(1,2*(N+1)), [zeros(1,N+1),flip(chord_distribution)], [r_R*Radius,flip(r_R*Radius)])% blade outline
+% scatter3(cp(:,1), cp(:,2), cp(:,3), 15, 'r', 'filled')  % control points
+% plot3(zeros(1,N+1),chord_distribution/4,r_R*Radius,'-+', 'Color', 'k')   % bound vortices
+% for i=1:N+1
+%     plot3([0, trail_points.x(i,:)], [chord_distribution(i)/4,trail_points.y(i,:)],[r_R(i)*Radius,trail_points.z(i,:)], 'k')
+% end
+% % axis equal
+% xlabel("x")
+% ylabel("y")
+% zlabel("z")
+% hold off
+% 
+% %% Calculate
+% %why do we also have r_R as an output?
+% [a, aline, r_R, Fnorm, Ftan, Gamma] = solveGamma(Uinf, N, Radius, cp, Influence_u, Influence_v, Influence_w, Omega, polar_alpha, polar_cl, polar_cd, cp_twist);
+% 
+% %% Post processing
+% temp = RootLocation_R + flip(b*(cos(theta)+1));  
+% dr = temp(2:end)-temp(1:end-1); % lengths of sections
+% 
+% CT_sections = Fnorm*NBlades.*dr/(0.5*Uinf^2*pi*Radius^2); % local thrust coefficient
+% CQ_sections = Ftan.*r_R*NBlades.*dr*Radius/(0.5*Uinf^3*pi*Radius^2);    % local torque coefficient
+% CP_sections = Ftan.*r_R*NBlades.*dr*Radius*Omega/(0.5*Uinf^3*pi*Radius^2); % local power coefficient  P=omega*Q
+% Gamma = Gamma/(pi*Uinf^2/(NBlades*Omega)); % non-dimensionalize circulation
+% 
+% CT = sum(dr.*Fnorm*NBlades/(0.5*Uinf^2*pi*Radius^2));     % total thrust coefficient
+% CP = sum(dr'.*Ftan.*r_R*NBlades*Radius*Omega/(0.5*Uinf^3*pi*Radius^2));    % total power coefficient
+% 
+% CT_all = sum(CT_sections, 'all');  % write to array of thrust coefficients
+% CQ_all = sum(CQ_sections, 'all');  % write to array of torque coefficients
+% CP_all = sum(CP_sections, 'all');  % write to array of power coefficients
+% 
+% %%%%%% SHOULD BE %%%%%%%%%
+% % CT = 0.6553;
+% % CQ = 0.2801;
+% % CP = 0.4482;
 
--- vortex_system.m
+++ vortex_system.m
@@ -1,19 +1,23 @@
-function [cp, bound, trail]=vortex_system(NBlades, theta_array,N, r_R, Radius, twist_distribution, chord_distribution)
+% FIX - double points in trail/ trail2
+% FIX - shift in points for trail2 not good
+function [cp, bound, trail]=vortex_system(Omega, NBlades, theta_array,N, r_R, Radius, twist_distribution, chord_distribution, Lw_D, Uwake)
+ts = 0.2; % maybe come up with a function for determining this
+t = [0:ts:Lw_D*2*Radius/Uwake];
 
-for n=1:NBlades
+for n=1:NBlades % loop over blades
     blade_azim = theta_array(n);
     
     % determine control point locations - at middle of segments
     r = (r_R(1:end-1)+r_R(2:end))/2*Radius;
     cp.x((n-1)*N+1:n*N) = zeros(N,1); % x-coordinate
-    cp.y((n-1)*N+1:n*N) = r*cos(blade_azim);     % y-coordinate
-    cp.z((n-1)*N+1:n*N) = r*sin(blade_azim)     % z-coordinate
+    cp.y((n-1)*N+1:n*N) = r*cos(2*pi-blade_azim);     % y-coordinate
+    cp.z((n-1)*N+1:n*N) = r*sin(2*pi-blade_azim)     % z-coordinate
     cp.twist((n-1)*N+1:n*N) = interp1(r_R, twist_distribution, r/Radius); % twist at control points
     
     % bound vortices
     bound.x((n-1)*(N+1)+1:n*(N+1)) = zeros(N+1,1); % x-coordinate
-    bound.y((n-1)*(N+1)+1:n*(N+1)) = r_R*Radius*cos(blade_azim);     % y-coordinate
-    bound.z((n-1)*(N+1)+1:n*(N+1)) = r_R*Radius*sin(blade_azim)     % z-coordinate
+    bound.y((n-1)*(N+1)+1:n*(N+1)) = r_R*Radius*cos(2*pi-blade_azim);     % y-coordinate
+    bound.z((n-1)*(N+1)+1:n*(N+1)) = r_R*Radius*sin(2*pi-blade_azim)     % z-coordinate
     
     %determine trailing vortices
     for i=1:N+1 % loop over bound vortices
@@ -21,31 +25,18 @@ for n=1:NBlades
         c = chord_distribution(i);
         beta = -deg2rad(twist_distribution(i));
         
-        % baseline
-        trail.x((n-1)*(N+1)+i) = bound.x((n-1)*(N+1)+i)+c*cos(beta);
-        trail.y((n-1)*(N+1)+i) = bound.y((n-1)*(N+1)+i)-c*sin(beta)*sin(blade_azim);
-        trail.z((n-1)*(N+1)+i) = bound.z((n-1)*(N+1)+i)+c*sin(beta)*cos(blade_azim);
+        % 1st trailing vortex, not sure about this
+        trailx((n-1)*(N+1)+i) = bound.x((n-1)*(N+1)+i)+c*cos(beta);
+        traily((n-1)*(N+1)+i) = bound.y((n-1)*(N+1)+i)-c*sin(beta)*sin(blade_azim);
+        trailz((n-1)*(N+1)+i) = bound.z((n-1)*(N+1)+i)+c*sin(beta)*cos(blade_azim);
         
-        % trailing vortices
-        %                 trail_x1 = 0 + chord_distribution(i)*sin(deg2rad(-twist_distribution(i)));
-        %                 trail_y1 = chord_distribution(i)/4 + chord_distribution(i)*cos(deg2rad(-twist_distribution(i)));
-        %                 trail_z1 = r_R(i)*Radius + 0;
-        %
-        
-        
-        %ts_func = @(ts) sqrt(ts^2*Uwake^2+r_R(i)^2*Radius^2*sin(Omega*ts)^2+r_R(i)^2*Radius^2*cos(Omega*ts)^2)-chord_distribution(i) ;
-        %ts = fsolve(ts_func,0.1);  % step so that we have wake discretized into chord lengths
-        %                 ts = 0.2; % maybe come up with a function for determining this
-        %                 t = [0:ts:Lw_D*2*Radius/Uwake];
-        %                 x = t*Uwake;
-        %                 y = r_R(i)*Radius*sin(Omega*t);
-        %                 z = r_R(i)*Radius*cos(Omega*t);
-        %                 trail.x(i,:) = [trail_x1, trail_x1+x];
-        %                 trail.y(i,:) = [trail_y1, trail_y1+y]; %[trail_y1, trail_y1+y];
-        %                 trail.z(i,:) = [trail_z1, z]; %[trail_z1, trail_z1+z];
+        x = t*Uwake;
+        y = r_R(i)*Radius*sin(Omega*t+pi/2+blade_azim);
+        z = r_R(i)*Radius*cos(Omega*t+pi/2+blade_azim);
+        trail.x((n-1)*(N+1)+i,:) = x+trailx((n-1)*(N+1)+i);
+        trail.y((n-1)*(N+1)+i,:) = y-c*sin(beta)*sin(blade_azim); %[trail_y1, trail_y1+y];
+        trail.z((n-1)*(N+1)+i,:) = z+c*sin(beta)*cos(blade_azim); %[trail_z1, trail_z1+z];
     end
-    
-    
 end
 
 % plot system - for checking
@@ -55,9 +46,13 @@ for n=1:NBlades
     plot3(bound.x((n-1)*(N+1)+1:n*(N+1)), bound.y((n-1)*(N+1)+1:n*(N+1)), bound.z((n-1)*(N+1)+1:n*(N+1)), '-k+')
     for i=1:N+1
         plot3([trail.x((n-1)*(N+1)+i), bound.x((n-1)*(N+1)+i)], [trail.y((n-1)*(N+1)+i), bound.y((n-1)*(N+1)+i)], [trail.z((n-1)*(N+1)+i), bound.z((n-1)*(N+1)+i)], '-k+')
+        plot3(trail.x((n-1)*(N+1)+i,:), trail.y((n-1)*(N+1)+i,:), trail.z((n-1)*(N+1)+i,:), '-b+')
     end
 end
-
+% for i=1:length(trail2.x(:,1))
+%     plot3(trail2.x(i,:), trail2.y(i,:), trail2.z(i,:), '-k+')
+% end
+% axis equal
 hold off
 xlabel("x")
 ylabel("y")
