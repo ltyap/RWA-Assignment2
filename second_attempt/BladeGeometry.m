b23531e750842e4d4f1486058b012c93353bae61
ltyap <litoong.yap@gmail.com>
2021-05-20 08:15:45 +0200

    updated files in second_attempt

--- second_attempt/BladeGeometry.m
+++ second_attempt/BladeGeometry.m
@@ -2,6 +2,6 @@ function [chord_distribution,twist_distribution] = BladeGeometry(r_R)
 %Returns the chord and twist distribution along the blade
 
 pitch = 2; % degrees
-chord_distribution = 3*(1-r_R)+1; % meters
-twist_distribution = -14*(1-r_R)+pitch; % degrees
+chord_distribution = 3*(1-r_R)+1; % [m]
+twist_distribution = -14*(1-r_R)+pitch; % [deg]
 end
