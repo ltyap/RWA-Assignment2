259c1ab0264992ee766038ec9d05100f3f7504d6
UlaCebula <u.golyska@gmail.com>
2021-05-25 15:53:24 +0200

    Small updates to single rotor main

--- second_attempt/main_single_rotor.m
+++ second_attempt/main_single_rotor.m
@@ -22,8 +22,9 @@ altitude = 0;   %[km]
 a_wake_bem = 0.2602;
 N = 20; % nr of blade segments
 spacing = 1; % 1- uniform, 0-cosine
-L=0;    % distance between 2 rotors
+L = 0;    % distance between 2 rotors
 sec_rot=0;  % is there a second rotor
+phase_diff = 0;
 Nrotations = 10; %number of rotations done by the rotor (length of wake)
 
 [CT,CP,CQ, results] = lifting_line_loop(rotor,windvel,N,spacing,L, sec_rot, a_wake_bem, Nrotations);
@@ -36,23 +37,38 @@ plot_BEM_vs_LLT(windvel, rotor.TSR, rotor.Radius, rotor.NBlades, rho);
 
 % assumed convection speed of wake
 a_wake = [0,0.25,a_wake_bem, 0.5,0.75];
+filenames={};
 for i=1:length(a_wake)
-    [CT_a(i),CP_a(i),CQ_a(i), results_a] = lifting_line_loop(N,spacing,L, sec_rot,a_wake(i), Nrotations);
+    [CT_a(i),CP_a(i),CQ_a(i), results] = lifting_line_loop(rotor, windvel, N,spacing,L, sec_rot, a_wake(i), Nrotations, phase_diff)
     filename = "results/results_llt_N"+N+"a_"+a_wake(i)+".mat";
-    save(filename,'results_a');
+    filenames{i} = filename;
+    save(filename,'results');
 end
-plot_compare(a_wake,N);
+var_change = "a_{wake}";% variable that changes - for plot legend
+plot_compare(filenames, a_wake, var_change, N, rotor, rho, windvel);
 
 % discretization of the blade (constant, cosine) azimuthal discretization 
 % (number of wake segments per rotation)
 spacing=[0,1];
+filenames={};
 for i=1:length(spacing)
-    [CT_cos(i),CP_cos(i),CQ_cos(i)] = lifting_line_loop(N,spacing(i),L, sec_rot,a_wake_bem);
+    [CT_cos(i),CP_cos(i),CQ_cos(i),results] = lifting_line_loop(rotor, windvel, N,spacing,L, sec_rot, a_wake_bem, Nrotations, phase_diff);
+    filename = "results/results_llt_N"+N+"cos_"+spacing(i)+".mat";
+    filenames{i} = filename;
+    save(filename,'results');
 end
-plot_compare;
+var_change = "spacing";% variable that changes - for plot legend
+plot_compare(filenames, spacing, var_change, N, rotor, rho, windvel);
+
+
 % length of the wake (number of rotations), including convergence of the solution with wake length
 spacing=1;
 Nrotations = [0.1, 1, 5,10,15];
-for i=1:length(L_D)
-    [CT_rot(i),CP_rot(i),CQ_rot(i),results_rot, conv(i)] = lifting_line_loop(N,spacing,L, sec_rot,a_wake_bem, Nrotations);
+for i=1:length(Nrotations)
+    [CT_rot(i),CP_rot(i),CQ_rot(i),results, conv(i)] = lifting_line_loop(rotor, windvel, N,spacing,L, sec_rot, a_wake_bem, Nrotations(i), phase_diff);
+    filename = "results/results_llt_N"+N+"rot_"+Nrotations(i)+".mat";
+    filenames{i} = filename;
+    save(filename,'results');
 end
+var_change = "N_{rotations}";% variable that changes - for plot legend
+plot_compare(filenames, Nrotations, var_change, N, rotor, rho, windvel);
